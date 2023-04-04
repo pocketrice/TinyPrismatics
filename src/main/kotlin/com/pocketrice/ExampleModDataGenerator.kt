862f8fddbd690a9581db82d2fc1ee42f24e47b57
PocketRice <xie.3@uaschools.org>
2023-04-03 22:38:13 -0400

    Add sample Fabric project
    See https://fabricmc.net/develop/template/.

---
+++ src/main/kotlin/com/pocketrice/ExampleModDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.pocketrice
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ExampleModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
